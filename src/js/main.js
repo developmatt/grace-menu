49ee1b5b1ba47938756a4d84fd8ab44960a8cde9
Matheus Godoi <matheusasdev@gmail.com>
2018-09-08 12:53:08 -0300

    Adding animations

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,32 @@
-console.log("webpack sass js");
+$(document).ready(function(){
+    $('.grace-menu-menu-toggler').click(function(){
+        $('.grace-menu-background').toggleClass("opened");
+        setTimeout(function(){
+            $('.grace-menu').toggleClass("opened");
+        }, 100)
+    });
+
+    $('.grace-menu-opener').click(function(){
+        $('.grace-menu-background').addClass("opened");
+        setTimeout(function(){
+            $('.grace-menu').addClass("opened");
+        }, 100)
+    });
+
+    $('.grace-menu-closer').click(function(){
+        $('.grace-menu-background').removeClass("opened");
+        setTimeout(function(){
+            $('.grace-menu').removeClass("opened");
+        }, 100)
+    });
+
+    let graceMenu = document.createElement('div');
+    graceMenu.classList.add('grace-menu');
+
+    let graceMenuBackground = document.createElement('div');
+    graceMenuBackground.classList.add("grace-menu-background");
+
+    $('.grace-menu-body').appendTo(graceMenu).css('opacity', '1');
+
+    $('.grace-menu-container').append(graceMenu).append(graceMenuBackground);
+});
